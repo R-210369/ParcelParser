4a0a484a0937788421c7a8d1f7259e368455725d
R-210369 <94705015+R-210369@users.noreply.github.com>
2021-11-27 12:47:37 +0100

    Update main.cpp
    Add loop

--- ParcelParser/main.cpp
+++ ParcelParser/main.cpp
@@ -4,13 +4,19 @@
 #include "Printa/printa.hpp"
 #include <iostream>
 
+#define string_equals_yes(x) x == "Y" || x == "y" || x == "Yes" || x == "yes" || x == "YES"
+#define string_equals_no(x) x == "N" || x == "n" || x == "No" || x == "no" || x == "no"
+
 double width, lenght, height;
+std::string _continue;
 
 auto main(int argc, char** argv) -> int
 {
+	A:
+	
 	printa->project<30>(E_("ParcelParser"));
 	SetConsoleTitleA(E("ParcelParser by 210369"));
-
+	
 	printa->print<loading>(E_("Package Width in cm: "));
 	std::cin >> width;
 
@@ -23,6 +29,16 @@ auto main(int argc, char** argv) -> int
 	printf(E("\n"));
 
 	ParcelParser* parcel;
+/*
+	Availeble Parcel Information:
+		parcel->category
+		parcel->ex_type
+		parcel->price
+		parcel->width
+		parcel->lenght
+		parcel->height
+		parcel->volume
+*/
 
 	if(lenght == 0)
 		parcel = new ParcelParser(width, height);
@@ -31,15 +47,33 @@ auto main(int argc, char** argv) -> int
 
 	parcel->print_info();
 
-	/*
-	Availeble Parcel Information:
-		parcel->category
-		parcel->ex_type
-		parcel->price 
-		parcel->width  
-		parcel->lenght 
-		parcel->height 
-		parcel->volume  
-	*/
-	_return();
+	B:
+
+	printa->print<loading>("Would you like to review another package?\n (Y/N): ");
+	std::cin >> _continue;
+ 
+	if (string_equals_yes(_continue)) {
+		delete parcel;
+		system("CLS");
+		goto A;
+	}
+		 
+	if (string_equals_no(_continue)) {
+		printa->print<ok>("Thank you for using, ParcelParser by R-210369");
+		for (int i = 0; i < 5; i++) {
+			Sleep(80);
+			std::cout << ".";
+		}
+		Sleep(3000);
+		exit(0);
+	}
+	else {
+		printa->print<fail>("Unknown Input\n");
+		Sleep(2000);
+		goto B;
+	}
+ 
+	printa->print<fail>("an unknown error occurred\n");
+	stay_open();
+	return -1;
 }
